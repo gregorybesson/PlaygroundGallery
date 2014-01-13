2073fbebaabfc66cb569cbf8f8fa9fb15cd75ee1
fozeek <dark.quent@free.fr>
2014-01-13 09:17:25 +0100

    CCC-83, remove vardump

--- src/PlaygroundGallery/Form/Category.php
+++ src/PlaygroundGallery/Form/Category.php
@@ -85,7 +85,6 @@ class Category extends ProvidesEventsForm
     */
     private function getWebsites()
     {
-        var_dump('hello');
         $websites = $this->getServiceManager()->get('playgroundcore_website_service')->getWebsiteMapper()->findAll();
         $websitesForm = array();
         foreach ($websites as $website) {
